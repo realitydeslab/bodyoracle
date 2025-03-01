a3abcb5f31d38ac7c31d96ea5c65a57f698fba9f
Ruoqi Wang <RachelWang_create@163.com>
2025-03-01 12:18:40 +0800

    update yolo.cs

--- Assets/Scripts/Yolo.cs
+++ Assets/Scripts/Yolo.cs
@@ -1,58 +1,99 @@
 using UnityEngine;
-using System.Collections.Generic;
 using Unity.Sentis;
+using UnityEngine.XR.ARFoundation;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class Yolo : MonoBehaviour
 {
-    public ModelAsset modelAsset;
+    [SerializeField] private ARCameraManager arCameraManager;
+    [SerializeField] private ModelAsset modelAsset;
     private Model runtimeModel;
     private Worker worker;
+    private int cam_width;
+    private int cam_height;
+    private int crop_size;
 
     void Start()
     {
         runtimeModel = ModelLoader.Load(modelAsset);
         worker = new Worker(runtimeModel, BackendType.GPUCompute);
 
-        CameraCapture.Init();
-        screenImage = GameObject.Find("ScreenImage").GetComponent<Image>();
+        arCameraManager = FindAnyObjectByType<ARCameraManager>();
+        arCameraManager.frameReceived += OnCameraFrameReceived;
     }
 
-    private Image screenImage;
-
-    void Update()
+    private void OnCameraFrameReceived(ARCameraFrameEventArgs args)
     {
-        WebCamTexture texture_cam = CameraCapture.GetCameraTexture();
-        screenImage.material.mainTexture = texture_cam;
-        if (texture_cam.width >= 100)
-        {
-            Tensor<float> inputTensor = PreprocessImage(texture_cam);
-            worker.Schedule(inputTensor);
-            Tensor<float> outputTensor = worker.PeekOutput() as Tensor<float>;
-            Debug.Log("outputTensor:");
-            Debug.Log(outputTensor);
-        }
+        if (args.textures.Count == 0) return;
+        Texture2D cameraTexture = args.textures[0];
+        cam_width = cameraTexture.width;
+        cam_height = cameraTexture.height;
+        crop_size = Mathf.Min(cam_width, cam_height);
+        Debug.Log("cameraTexture format: " + cameraTexture.format);
+
+        Tensor<float> inputTensor = PreprocessTexture(cameraTexture);
+
+        worker.Schedule(inputTensor);
+        Tensor<float> outputTensor = worker.PeekOutput() as Tensor<float>;
+
+        ProcessOutput(outputTensor);
     }
 
-    Tensor<float> PreprocessImage(WebCamTexture camera_texture, int target = 640)
+    private Tensor<float> PreprocessTexture(Texture2D texture)
     {
-        int source_w = camera_texture.width;
-        int source_h = camera_texture.height;
-        int crop_size = Mathf.Min(source_w, source_h); // TEST!!!
-        int start_x = (source_w - crop_size) / 2;
-        int start_y = (source_h - crop_size) / 2;
+        // Crop
+        int start_x = (cam_width - crop_size) / 2;
+        int start_y = (cam_height - crop_size) / 2;
 
-        Texture2D texture_cropped = new Texture2D(crop_size, crop_size, TextureFormat.RGB24, false);
+        Texture2D texture_cropped = new Texture2D(crop_size, crop_size, texture.format, false);
 
-        Color[] pixels = camera_texture.GetPixels(start_x, start_y, crop_size, crop_size);
+        Color[] pixels = texture.GetPixels(start_x, start_y, crop_size, crop_size);
         texture_cropped.SetPixels(pixels);
         texture_cropped.Apply();
 
-        Texture2D texture_resized = new Texture2D(target, target, TextureFormat.RGB24, false);
+        // Resize
+        Texture2D texture_resized = new Texture2D(640, 640, texture.format, false);
         Graphics.ConvertTexture(texture_cropped, texture_resized);
-        
-        // Convert to a tensor
-        Tensor<float> tensor = TextureConverter.ToTensor(texture_resized, target, target, 3);
-        return tensor;
+
+        // Adjust color format: BGRA32 to RGB
+        Color32[] pixels32 = texture_resized.GetPixels32();
+        float[] rgbData = new float[640 * 640 * 3];
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            rgbData[i * 3] = pixels[i].r / 255.0f;
+            rgbData[i * 3 + 1] = pixels[i].g / 255.0f;
+            rgbData[i * 3 + 2] = pixels[i].b / 255.0f;
+        }
+
+        return new Tensor<float>(new TensorShape(1, 3, 640, 640), rgbData);
+    }
+
+    private void ProcessOutput(Tensor<float> output)
+    {
+        float[] data = output.DownloadToArray();
+        Debug.Log(data.Length);
+
+        float conf_threshold = 0.5f;
+        for (int i = 0; i < data.Length; i += 57)
+        {
+            int cls = (int)data[i];
+            float conf = data[i + 5];
+            if (conf < conf_threshold)
+            {
+                continue;
+            }
+
+            float x1 = data[i + 1] * crop_size + (cam_width - crop_size) / 2;
+            float y1 = (1 - data[i + 2]) * crop_size + (cam_height - crop_size) / 2;
+            float x2 = data[i + 3] * crop_size + (cam_width - crop_size) / 2;
+            float y2 = (1 - data[i + 4]) * crop_size + (cam_height - crop_size) / 2;
+            float w = x2 - x1;
+            float h = y2 - y1;
+            float cx = (x1 + x2) / 2.0f;
+            float cy = (y1 + y2) / 2.0f;
+
+            // TODO: display images
+        }
     }
 }
